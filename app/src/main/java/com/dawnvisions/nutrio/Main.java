0ab64eb9954a204b7a4498f41c8c458fbd972339
Elizabeth <elizabeth@dawnvisions.com>
2018-09-27 12:32:19 -0500

    Navigation Bar
    Navigation bar set up with initial options

--- app/src/main/java/com/dawnvisions/nutrio/Main.java
+++ app/src/main/java/com/dawnvisions/nutrio/Main.java
@@ -1,9 +1,16 @@
 package com.dawnvisions.nutrio;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class Main extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener
 {
 
     @Override
@@ -11,5 +18,24 @@ public class Main extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+    {
+        return false;
     }
 }
